e577af681274389c9e9e448a4060b998796fb1b3
ben <noahbenaas@gmail.com>
2021-11-16 22:06:35 +0100

    update some styling

--- news-app/src/components/NewsBody.js
+++ news-app/src/components/NewsBody.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Container, Image, Card, Button } from "react-bootstrap/";
+import { Container, Image, Card } from "react-bootstrap/";
 
 export default function NewsBody() {
   const [col1, setData] = useState([]);
+  const [col2, setData2] = useState([]);
+  const [title, setTitle] = useState(
+    window.localStorage.getItem(`{item.title}`) || ""
+  );
+
+  const handleChange = (event) => setTitle(event.target.value);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,11 +17,21 @@ export default function NewsBody() {
       );
       const data = await res.json();
       const col1 = data[0][0].columns;
+      const col2 = data[0][1].columns;
+      console.log("col2", col2);
       setData(col1);
+      setData2(col2);
     };
     fetchData();
-  }, [setData]);
+  }, [setData, setData2]);
 
+  useEffect(() => {
+    window.localStorage.setItem(`{item.title}`, title);
+  });
+
+  const remove = () => {
+    localStorage.removeItem("{item.title}");
+  };
   return (
     <Container className="NewsHolder">
       <div className="row">
@@ -24,8 +40,51 @@ export default function NewsBody() {
             <Card>
               <Image style={{ height: "10rem" }} src={item.imageUrl} />
               <Card.Body>
-                <input value={item.title} />
+                <form>
+                  <label htmlFor="{item.title}">Title: </label>
+                  <input
+                    value={item.title}
+                    onChange={handleChange}
+                    id="{item.title}"
+                  />
+                  <br></br>
+                  <a href="/">{item.url}</a>
+                </form>
+                {item.title ? (
+                  <strong>{item.title} </strong>
+                ) : (
+                  "Please type new title"
+                )}
               </Card.Body>
+              <button onClick={remove}>Remove</button>
+            </Card>
+            <p></p>
+          </div>
+        ))}
+      </div>
+      <div className="row row-col2">
+        {col2.map((item) => (
+          <div style={{ height: "100%" }} className="col-md-6  ">
+            <Card>
+              <Image style={{ height: "16rem" }} src={item.imageUrl} />
+              <Card.Body>
+                <form>
+                  <label htmlFor="{item.title}">Title:{item.title} </label>
+                  <input
+                    value={item.title}
+                    onChange={handleChange}
+                    id="{item.title}"
+                  />
+                  <br></br>
+                  <a href="/">{item.url}</a>
+                </form>
+                {item.title ? (
+                  <strong>{item.title} </strong>
+                ) : (
+                  "Please type new title"
+                )}
+              </Card.Body>
+              <button onClick={remove}>Remove</button>
             </Card>
             <p></p>
           </div>
